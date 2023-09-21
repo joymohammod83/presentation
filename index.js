dc34a32ce44447165d14e7b57ce3f2c4cde5a020
joymohammod83 <mdjoysarkar83@gmail.com>
2023-09-21 22:35:01 +0600

    js

---
+++ index.js
@@ -0,0 +1,89 @@
+console.log("hellow mr joybhai")
+
+let myFriend = "sojol";
+console.log(myFriend)
+
+let cow = "sajol";
+ console.log(cow);
+
+
+ let cash = 500;
+ let cash1 = 400;
+ let cash2 = 120;
+ console.log(cash + cash1 + cash2);
+
+ let bkasAdd = 5000;
+ let cashOut = 3500;
+ let blance =  bkasAdd-cashOut;
+ console.log("sum =", blance);
+
+ 
+ let mango = 12;
+ let person = 6;
+ let devag = mango / person;
+ console.log(devag);
+
+ let bhuyaBhill = 3000;
+ let totalPerson = 5;
+ let perPerson = bhuyaBhill / totalPerson;
+ console.log(perPerson);
+
+ let per_mill_cost = 42;
+ let tottal_mill = 50;
+ let tottal_mill_cost = per_mill_cost * tottal_mill;
+ console.log(tottal_mill_cost);
+
+ let sum1 = 20;
+ let sum2 = 3;
+ let multi = sum1 % sum2;
+ console.log(multi);
+
+let scorebord = 199;
+scorebord++;
+console.log(scorebord);
+
+let myGf = 1;
+myGf--;
+console.log( "broken", myGf);
+
+let score = 210;
+score = score + 4;
+score = score + 2;
+score = score + 1;
+score = score + 6;
+console.log(score);
+
+let pockhet_money = 100;
+pockhet_money += 50;
+pockhet_money -= 49;
+pockhet_money *= 3;
+console.log(pockhet_money);
+
+let fristNmame = "joy";
+let lastName = "mohammod";
+let fullNmae = fristNmame + " " + lastName;
+console.log(fullNmae);
+
+let price = 25;
+let price1 = '30';
+let solve = parseInt(price1);
+let totalPrice = price + solve;
+console.log(totalPrice);
+
+let j1 = 19;
+let j2 = 29;
+if ( j1 == j2){
+    console.log("succes");
+}
+else {
+    console.log("failed");
+}
+
+let password = "iloveyou";
+let gmail = "i love you @ gmail.com";
+if ("iloveyou" == password && "i love you @ gmail.com" == gmail ){
+    console.log("showing your profilr");
+}
+else{
+    console.log("worng password");
+}
